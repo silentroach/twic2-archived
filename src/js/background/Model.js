9b5133215f1c0d2e7a926697de1600b472ca4e27
Kalashnikov Igor <igor.kalashnikov@me.com>
2014-01-06 02:00:44 +0400

    model isfresh

--- src/js/background/Model.js
+++ src/js/background/Model.js
@@ -1,5 +1,6 @@
 twic.Model = function() {
 	this.id = null;
+	this.updateTime = null;
 };
 
 twic.Model.prototype.fillFromJSON = function(json) {
@@ -9,19 +10,30 @@ twic.Model.prototype.fillFromJSON = function(json) {
 twic.Model.prototype.serialize = function() {
 	return {
 		'id': this.id,
-		'updateTime': Math.round(+new Date() / 1000)
+		'updateTime': this.updateTime
 	};
 };
 
 twic.Model.prototype.deserialize = function(data) {
 	this.id = data['id'];
+	this.updateTime = data['updateTime'];
 };
 
 twic.Model.prototype.save = function(callback) {
 	var
 		model = this;
 
+	model.updateTime = Math.round(+new Date() / 1000);
+
 	twic.db.put(this.constructor.collectionName, model.serialize(), function() {
 		callback.call(model);
 	} );
+};
+
+twic.Model.prototype.isFresh = function() {
+	var
+		freshTime = this.constructor.freshTime || 60 * 60,
+		now = Math.round(+new Date() / 1000);
+
+	return this.updateTime + freshTime
 };
